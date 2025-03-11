e617824c4436088226561f3db60e0239579dc012
Chipinkoskin <118463466+verikirill@users.noreply.github.com>
2025-03-11 19:40:46 +0300

    final sber

--- web_app/app.py
+++ web_app/app.py
@@ -345,4 +345,4 @@ async def upload_file(
 # Запуск приложения
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=True) 
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
